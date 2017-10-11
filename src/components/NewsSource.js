9844ba6521b489829f60188be7e04e0d67f51e01
Andrew Ratnikov <ratnikov.am@gmail.com>
2017-10-11 17:24:24 +0300

    simple styles for articles

--- src/components/NewsSource.js
+++ src/components/NewsSource.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NewsSource = ({ source }) => {
     console.log(source);
     return (
-        <div className="news-sources__item n-source">
+        <div className="news-sources__item n-source n-source--active">
             <h5 className="n-source__title">{source.name}</h5>
             <p>{source.description}</p>
             <p className="n-source__labels">
